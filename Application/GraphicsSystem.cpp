ffcc7dccd93e6c3954b0b4ef2e4893882d299d95
FatGarfieldjteng <fatgarfieldjteng@hotmail.com>
2023-02-22 22:35:10 +0800

    Add Rendering main control class

---
+++ Application/GraphicsSystem.cpp
@@ -0,0 +1,11 @@
+#include "GraphicsSystem.h"
+
+GraphicsSystem::GraphicsSystem()
+{
+
+}
+
+GraphicsSystem::~GraphicsSystem()
+{
+
+}
---
+++ Application/GraphicsSystem.h
@@ -0,0 +1,6 @@
+#pragma once
+class GraphicsSystem
+{
+	GraphicsSystem();
+	~GraphicsSystem();
+};
