18b52b649854dc1912315706787c956320407cf1
tengj <jun.teng@autodesk.com>
2023-02-23 11:31:51 +0800

    Link GLFW to vulkanInstance, add initial validation layer query function

--- Application/GraphicsSystem.cpp
+++ Application/GraphicsSystem.cpp
@@ -1,5 +1,7 @@
 #include "GraphicsSystem.h"
 
+#include <stdexcept>
+
 GraphicsSystem::GraphicsSystem()
 {
 
@@ -12,10 +14,40 @@ GraphicsSystem::~GraphicsSystem()
 
 void GraphicsSystem::run() 
 {
+    
     initWindow();
     initVulkan();
     mainLoop();
     cleanup();
+}
+
+void GraphicsSystem::createInstance()
+{
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "First Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+    createInfo.enabledLayerCount = 0;
+
+    if (vkCreateInstance(&createInfo, nullptr, &mVkInstance) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create Vulkan instance!");
+    }
+
 }
 
 void GraphicsSystem::initWindow()
@@ -28,7 +60,7 @@ void GraphicsSystem::initWindow()
 
 void GraphicsSystem::initVulkan() 
 {
-
+    createInstance();
 }
 
 void GraphicsSystem::mainLoop() 
@@ -40,7 +72,20 @@ void GraphicsSystem::mainLoop()
 
 void GraphicsSystem::cleanup() 
 {
+    vkDestroyInstance(mVkInstance, nullptr);
+
     glfwDestroyWindow(mWindow);
 
     glfwTerminate();
+}
+
+bool GraphicsSystem::checkValidationLayerSupport()
+{
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    return false;
 }
--- Application/GraphicsSystem.h
+++ Application/GraphicsSystem.h
@@ -3,6 +3,9 @@
 #include "GraphicsSystem.h"
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+
+#include <vector>
+
 class GraphicsSystem
 {
 public:
@@ -14,6 +17,10 @@ public:
     void run();
 
 private:
+
+    // create Vulkan instance
+    void createInstance();
+
     void initWindow();
 
     void initVulkan();
@@ -23,6 +30,22 @@ private:
     void cleanup();
 
 private:
+    bool checkValidationLayerSupport();
+
+private:
+
+    VkInstance mVkInstance = nullptr;
+
+    const std::vector<const char*> validationLayers = {
+        "VK_LAYER_KHRONOS_validation"
+    };
+
+#ifdef NDEBUG
+    const bool enableValidationLayers = false;
+#else
+    const bool enableValidationLayers = true;
+#endif
+
     const uint32_t WIDTH = 1024;
     const uint32_t HEIGHT = 768;
     GLFWwindow* mWindow = nullptr;
