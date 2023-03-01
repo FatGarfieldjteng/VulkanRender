1a7de1e04cfe5afd398bb079017662b248ab82a6
FatGarfieldjteng <fatgarfieldjteng@hotmail.com>
2023-03-01 23:24:51 +0800

    Add initial code for creating Command objects, compiling error exists

--- Application/Command.cpp
+++ Application/Command.cpp
@@ -1,4 +1,6 @@
 #include "Command.h"
+#include "Queue.h"
+#include <stdexcept>
 
 Command::Command()
 {
@@ -8,4 +10,32 @@ Command::Command()
 Command::~Command()
 {
 
+}
+
+void Command::create()
+{
+    // create CommandPool
+    Queue::QueueFamilyIndices queueFamilyIndices = Queue::findQueueFamilies(physicalDevice);
+
+    VkCommandPoolCreateInfo poolInfo{};
+    poolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+    poolInfo.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+    poolInfo.queueFamilyIndex = queueFamilyIndices.graphicsFamily.value();
+
+    if (vkCreateCommandPool(device, &poolInfo, nullptr, &mCommandPool) != VK_SUCCESS)
+    {
+        throw std::runtime_error("failed to create command pool!");
+    }
+
+    // create CommandBuffer
+    VkCommandBufferAllocateInfo allocInfo{};
+    allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    allocInfo.commandPool = mCommandPool;
+    allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    allocInfo.commandBufferCount = 1;
+
+    if (vkAllocateCommandBuffers(device, &allocInfo, &mCommandBuffer) != VK_SUCCESS) 
+    {
+        throw std::runtime_error("failed to allocate command buffers!");
+    }
 }
--- Application/Command.h
+++ Application/Command.h
@@ -1,7 +1,18 @@
 #pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 class Command
 {
 public:
 	Command();
 	~Command();
+
+public:
+	void create();
+
+private:
+	VkCommandPool mCommandPool = VK_NULL_HANDLE;
+	VkCommandBuffer mCommandBuffer = VK_NULL_HANDLE;
 };
